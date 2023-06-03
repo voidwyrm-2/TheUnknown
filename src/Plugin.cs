2646ff34a518827e8c93caf1b5de059243a59f59
AutobotCoder <AutobotCoder@DESKTOP-4IO5JNK>
2023-06-03 00:25:33 -0400

    added hooks flight up slow flight up slow flight in direction BABY YOUE'RE A FIREWORK self-destruct (unfinished)longer air capacity

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -14,7 +14,7 @@ using MonoMod.Cil;
 using IL;
 //using static UnityEngine.Input;
 
-namespace SlugTemplate
+namespace NuclearPasta.TheUnknown
 {
     [BepInPlugin(MOD_ID, "The Unknown", "0.1.0")]
     class Plugin : BaseUnityPlugin
@@ -26,71 +26,205 @@ namespace SlugTemplate
         // Add hooks
         public void OnEnable()
         {
-            On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+            //On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
 
             // Put your custom hooks here!
-            On.Player.checkInput += Player_checkInput1;
-            On.Player.checkInput += Player_checkInput2;
-        }
-
-        private void Player_checkInput1(On.Player.orig_checkInput orig, Player self)
-        {
-            orig(self);
-            if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
-            {
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    self.mainBodyChunk.vel.y += 100f;
-                }
-                else
-                {
-                    return;
-                }
-            }
-        }
-
-        private void Player_checkInput2(On.Player.orig_checkInput orig, Player self)
-        {
-            orig(self);
-            if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
-            {
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    self.mainBodyChunk.vel.y += 25f;
-                }
-                else
-                {
-                    return;
-                }
-            }
+            On.Player.checkInput += Player_FlyUp;
+            On.Player.checkInput += Player_FlyUpLess;
+            //On.Player.checkInput += Player_FlyUp;
+            On.Player.checkInput += Player_FlyRight;
+            On.Player.checkInput += Player_FlyLeft;
+            On.Player.checkInput += Player_FlyDown;
+            On.Player.checkInput += Player_FlyUpvelxy1;
+            On.Player.checkInput += Player_FlyUpvelxy2;
+            On.Player.checkInput += Player_FlyUpvelxy3;
+            On.Player.checkInput += Player_BootlegAscension;
+            On.Player.Die += Player_SelfDestruct;
+            //On.Player.checkInput += Player_Ribulet;
         }
 
         /*
-        private void Player_checkInput11(On.Player.orig_checkInput orig, Player self)
+        private void Player_Ribulet(On.Player.orig_checkInput orig, Player self)
         {
             orig(self);
-            if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
-            {
-                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.W))
-                {
-                    self.mainBodyChunk.vel.y += 10f;
-                }
-                else
-                {
-                    return;
-                }
-            }
         }
         */
 
-        private void Player_checkInput12(On.Player.orig_checkInput orig, Player self)
+
+
+        private void Player_SelfDestruct(On.Player.orig_Die orig, Player self)
+        {
+            orig(self);
+            if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == AbstractPhysicalObject.AbstractObjectType.ScavengerBomb)
+            {
+                if (Input.GetKey(KeyCode.LeftControl))
+                {
+                    if (!self.dead)
+                    {
+                        var room = self.room;
+                        var pos = self.mainBodyChunk.pos;
+                        var color = self.ShortCutColor();
+                        room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 20f, 1000f, 0.75f, self, 0.7f, 160f, 1f));
+                        room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                        room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                        room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                        room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+
+                        room.ScreenMovement(pos, default, 1.3f);
+                        room.PlaySound(SoundID.Bomb_Explode, pos);
+                        room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+                        self.room.abstractRoom.creatures[0].realizedCreature.Die();
+                    }
+                }
+            }
+        }
+
+            private void Player_BootlegAscension(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == AbstractPhysicalObject.AbstractObjectType.ScavengerBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        var room = self.room;
+                        var pos = self.mainBodyChunk.pos;
+                        var color = self.ShortCutColor();
+                        room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 10f, 1000f, 0.25f, self, 0.7f, 160f, 1f));
+                        room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                        room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                        room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                        room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+
+                        room.ScreenMovement(pos, default, 1.3f);
+                        room.PlaySound(SoundID.Bomb_Explode, pos);
+                        room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+                    }
+                }
+            }
+
+            private void Player_FlyUp(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        self.mainBodyChunk.vel.y += 100f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+
+
+            private void Player_FlyUpLess(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftControl))
+                    {
+                        self.mainBodyChunk.vel.y += 10f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            /*
+            private void Player_FlyUp(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.W))
+                    {
+                        self.mainBodyChunk.vel.y += 10f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            */
+
+            private void Player_FlyRight(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.D))
+                    {
+                        self.mainBodyChunk.vel.x += 10f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            private void Player_FlyLeft(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.A))
+                    {
+                        self.mainBodyChunk.vel.x += -10f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            private void Player_FlyDown(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.S))
+                    {
+                        self.mainBodyChunk.vel.y += -10f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            private void Player_FlyUpvelxy1(On.Player.orig_checkInput orig, Player self)
+            {
+                orig(self);
+                if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
+                {
+                    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.S))
+                    {
+                        self.mainBodyChunk.vel.y += 2f;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        private void Player_FlyUpvelxy2(On.Player.orig_checkInput orig, Player self)
         {
             orig(self);
             if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
             {
-                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.D))
+                if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.A))
                 {
-                    self.mainBodyChunk.vel.x += 25f;
+                    self.mainBodyChunk.vel.y += 2f;
                 }
                 else
                 {
@@ -98,42 +232,20 @@ namespace SlugTemplate
                 }
             }
         }
-
-        private void Player_checkInput13(On.Player.orig_checkInput orig, Player self)
+        private void Player_FlyUpvelxy3(On.Player.orig_checkInput orig, Player self)
         {
             orig(self);
             if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
             {
-                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.A))
+                if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.D))
                 {
-                    self.mainBodyChunk.vel.x += -25f;
+                    self.mainBodyChunk.vel.y += 2f;
                 }
                 else
                 {
                     return;
                 }
             }
-        }
-
-        private void Player_checkInput14(On.Player.orig_checkInput orig, Player self)
-        {
-            orig(self);
-            if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == MoreSlugcatsEnums.AbstractObjectType.SingularityBomb)
-            {
-                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.S))
-                {
-                    self.mainBodyChunk.vel.y += -25f;
-                }
-                else
-                {
-                    return;
-                }
-            }
-        }
-
-        // Load any resources, such as sprites or sounds
-        private void LoadResources(RainWorld rainWorld)
-        {
         }
 
 
