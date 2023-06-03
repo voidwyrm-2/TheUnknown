fd1e3604c6053ad9b259311945dbf460c41a9a9c
AutobotCoder <AutobotCoder@DESKTOP-4IO5JNK>
2023-06-03 12:15:07 -0400

    commented out beta hooks

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -39,45 +39,52 @@ namespace NuclearPasta.TheUnknown
             On.Player.checkInput += Player_FlyUpvelxy2;
             On.Player.checkInput += Player_FlyUpvelxy3;
             On.Player.checkInput += Player_BootlegAscension;
-            On.Player.Die += Player_SelfDestruct;
-            //On.Player.checkInput += Player_Ribulet;
+            //On.Player.Die += Player_SelfDestruct;
+            //On.Player.AerobicIncrease += Player_Ribulet;
         }
 
         /*
-        private void Player_Ribulet(On.Player.orig_checkInput orig, Player self)
+        private void Player_Ribulet(On.Player.orig_AerobicIncrease orig, Player self, float f)
         {
-            orig(self);
+            orig(self, f);
+            int i = 1000;
+            if(self.SlugCatClass == MySlugcat && self.objectInStomach?.type == AbstractPhysicalObject.AbstractObjectType.BubbleGrass)
+            {
+                self.airInLungs = i;
+            }
         }
         */
 
 
-
+        /*
         private void Player_SelfDestruct(On.Player.orig_Die orig, Player self)
         {
             orig(self);
+            bool wasDead = self.dead;
             if (self.SlugCatClass == MySlugcat && self.objectInStomach?.type == AbstractPhysicalObject.AbstractObjectType.ScavengerBomb)
             {
                 if (Input.GetKey(KeyCode.LeftControl))
                 {
-                    if (!self.dead)
+                    if (!wasDead && self.dead)
                     {
                         var room = self.room;
                         var pos = self.mainBodyChunk.pos;
                         var color = self.ShortCutColor();
-                        room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 20f, 1000f, 0.75f, self, 0.7f, 160f, 1f));
+                        room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 42f, 10000f, 0.75f, self, 0.7f, 160f, 1f));
                         room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
                         room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
                         room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
-                        room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+                        room.AddObject(new ShockWave(pos, 660f, 0.45f, 7, false));
 
                         room.ScreenMovement(pos, default, 1.3f);
                         room.PlaySound(SoundID.Bomb_Explode, pos);
                         room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
-                        self.room.abstractRoom.creatures[0].realizedCreature.Die();
+                        self.Die();
                     }
                 }
             }
         }
+        */
 
             private void Player_BootlegAscension(On.Player.orig_checkInput orig, Player self)
             {
